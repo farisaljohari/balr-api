e78c9f1c541be7be21cc6c71c51392c7ded399af
farisaljohari <farisaljohari@gmail.com>
2025-08-11 01:46:00 -0600

    remove tuya configuration from common config

--- libs/common/src/config/index.ts
+++ libs/common/src/config/index.ts
@@ -1,5 +1,4 @@
 import emailConfig from './email.config';
 import superAdminConfig from './super.admin.config';
-import tuyaConfig from './tuya.config';
 
-export default [emailConfig, superAdminConfig, tuyaConfig];
+export default [emailConfig, superAdminConfig];
